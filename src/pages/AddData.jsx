c81ba6f408310e5bd7876f975ded2ff2991b14be
Sayyed Irfan <sayyedirfan255@gmail.com>
2024-03-28 17:16:24 +0530

    post and read method

--- src/pages/AddData.jsx
+++ src/pages/AddData.jsx
@@ -1,66 +1,114 @@
+import Axios from "axios";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
-import Row from "react-bootstrap/Row";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function AddData() {
   const navigate = useNavigate();
 
-  const baseUrl = 'https://6603fb2b2393662c31d04b5f.mockapi.io/crud'
-  
-  const [validated, setValidated] = useState(false);
+  const baseUrl = "https://6603fb2b2393662c31d04b5f.mockapi.io/crud";
+
+  const [data, setData] = useState({
+    name: "",
+    mobile: "",
+    email: "",
+    address: "",
+  });
+
+  const { name, mobile, email, address } = data;
+
+  const handleChange = (e) => {
+    // let name = e.target.name
+
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+    event.preventDefault();
+    console.log(data);
 
-    setValidated(true);
+    Axios.post(baseUrl, {
+      name,
+      mobile,
+      email,
+      address,
+    }).then((res) => {
+      setData({
+        name: "",
+        mobile: "",
+        email: "",
+        address: "",
+      });
+
+      setTimeout(() => {
+        navigate("/");
+      }, 2000);
+
+      console.log(res);
+      toast.success("Data Successfully Added");
+      // alert("Data Successfully Added");
+    });
   };
 
   return (
     <>
       <h1 className="text-center text-danger">ADD DATA</h1>
       <hr />
-      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <ToastContainer />
+      <Form onSubmit={handleSubmit}>
         <Form.Group as={Col} md="6" className="mb-4">
           <Form.Label>Enter name</Form.Label>
-          <Form.Control required type="text" placeholder="Enter name" />
-          <Form.Control.Feedback type="invalid">
-            please enter your name!
-          </Form.Control.Feedback>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Enter name"
+            value={name}
+            onChange={handleChange}
+            name="name"
+          />
         </Form.Group>
 
         <Form.Group as={Col} md="6" className="mb-4">
           <Form.Label>Enter Mobile</Form.Label>
-          <Form.Control required type="number" placeholder="Enter mobile" />
-          <Form.Control.Feedback type="invalid">
-            Please enter your mobile
-          </Form.Control.Feedback>
+          <Form.Control
+            required
+            type="number"
+            placeholder="Enter mobile"
+            value={mobile}
+            onChange={handleChange}
+            name="mobile"
+          />
         </Form.Group>
 
         <Form.Group as={Col} md="6" className="mb-4">
           <Form.Label>Enter Email</Form.Label>
-          <Form.Control required type="email" placeholder="Enter email" />
-          <Form.Control.Feedback type="invalid">
-            Please enter your email
-          </Form.Control.Feedback>
+          <Form.Control
+            required
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={handleChange}
+            name="email"
+          />
         </Form.Group>
 
         <Form.Group as={Col} md="6" className="mb-4">
           <Form.Label>Enter Address</Form.Label>
-          <Form.Control required type="text" placeholder="Enter address" />
-          <Form.Control.Feedback type="invalid">
-            Please enter your adress
-          </Form.Control.Feedback>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Enter address"
+            value={address}
+            onChange={handleChange}
+            name="address"
+          />
         </Form.Group>
 
-        <Button type="submit" onClick={()=>navigate('/')}>Submit Data</Button>
+        <Button type="submit">Submit Data</Button>
       </Form>
     </>
   );
--- src/pages/ViewData.jsx
+++ src/pages/ViewData.jsx
@@ -1,56 +1,58 @@
-import React from "react";
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const ViewData = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
+  const [data, setData] = useState([]);
+
+  const baseUrl = "https://6603fb2b2393662c31d04b5f.mockapi.io/crud";
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const result = await Axios.get(baseUrl);
+      console.log(result.data);
+      setData(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(data);
   return (
     <div>
       <h1 className="text-center text-danger">USER DETAILS</h1>
       <div className="text-end">
-        <button className="btn btn-success" onClick={()=>navigate('/addData')} >Add Data</button>
+        <button
+          className="btn btn-success"
+          onClick={() => navigate("/addData")}
+        >
+          Add Data
+        </button>
       </div>
       <hr />
       <div className="row">
-        <div className="col-md-6">
-          <div className="card-style">
-            <h3>User Id :</h3>
-            <hr />
-            <h3>Name: </h3>
-            <h3>Mobile: </h3>
-            <h3>Email: </h3>
-            <h3>Address: </h3>'
-            <hr />
-            <button className="btn btn-info">Update</button>
-            <button className="btn btn-danger ms-3">Delete</button>
-          </div>
-        </div>
-        <div className="col-md-6">
-          <div className="card-style mb-4">
-            <h3>User Id :</h3>
-            <hr />
-            <h3>Name: </h3>
-            <h3>Mobile: </h3>
-            <h3>Email: </h3>
-            <h3>Address: </h3>'
-            <hr />
-            <button className="btn btn-info" onClick={()=>navigate('/updateData')}>Update</button>
-            <button className="btn btn-danger ms-3">Delete</button>
-          </div>
-        </div>
-        <div className="col-md-6">
-          <div className="card-style">
-            <h3>User Id :</h3>
-            <hr />
-            <h3>Name: </h3>
-            <h3>Mobile: </h3>
-            <h3>Email: </h3>
-            <h3>Address: </h3>'
-            <hr />
-            <button className="btn btn-info">Update</button>
-            <button className="btn btn-danger ms-3">Delete</button>
-          </div>
-        </div>
+        {data.map((items) => {
+          return (
+            <div className="col-md-6">
+              <div className="card-style mb-4">
+                <h3>User Id : {items.id}</h3>
+                <hr />
+                <h3>Name: {items.name} </h3>
+                <h3>Mobile: {items.mobile} </h3>
+                <h3>Email: {items.email} </h3>
+                <h3>Address: {items.address} </h3>'
+                <hr />
+                <button className="btn btn-info">Update</button>
+                <button className="btn btn-danger ms-3">Delete</button>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
